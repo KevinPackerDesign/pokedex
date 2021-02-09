f1f930562ba62c34323077a7822eb86a0c51b7c3
Kevinpacker <kevinpackerdesign@gmail.com.com>
2021-02-08 19:32:26 -0800

    added files
    added the pokedex file and added the required favoriteFood variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,22 @@
-let myName=('Kevin');
+let myName = "Kevin";
 document.write(myName);
 
-myName= 'Kevin Packer';
+myName = "Kevin Packer";
 document.write(myName);
 
 let simpleAddition = 2 + 2;
 document.write(simpleAddition);
+
 //let size = 100;
 //let doubleSize = size * 2;
 //document.write(doubleSize);
+
 let size = 200;
 let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
+let minSize = doubleSize * 2 - size / 2;
 document.write(minSize);
+
+let favoritFood = "Steak";
+document.write(favoritFood);
+
+alert("Hello World");
