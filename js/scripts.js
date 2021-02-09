a39981b131466a22300c72f0fbcb38cf815e7f4c
Kevinpacker <kevinpackerdesign@gmail.com.com>
2021-02-08 19:20:12 -0800

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+let myName=('Kevin');
+document.write(myName);
+
+myName= 'Kevin Packer';
+document.write(myName);
+
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+//let size = 100;
+//let doubleSize = size * 2;
+//document.write(doubleSize);
+let size = 200;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
