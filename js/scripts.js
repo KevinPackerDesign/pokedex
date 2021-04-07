af8f6dbe1e49b41c584d20d7eb25994d936efb37
KevinPackerDesign <kevinpacker@rocketmail.com>
2021-04-06 19:41:00 -0700

    adding search and color to pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -132,6 +132,33 @@ let pokemonRepository = (function () {
       "<p>" + "abilities : " + pokemon.abilities + "</p>"
     );
 
+    if (pokemon.types.includes("grass")) {
+        $(".modal-header").css("background-color", "rgb(120, 200, 80)");
+      } else if (pokemon.types.includes("fire")) {
+        $(".modal-header").css("background-color", "rgb(240, 128, 48)");
+      } else if (pokemon.types.includes("poison")) {
+        $(".modal-header").css("background-color", "rgb(168, 144, 240)");
+      } else if (pokemon.types.includes("water")) {
+        $(".modal-header").css("background-color", "rgb(104, 144, 240)");
+      } else if (pokemon.types.includes("bug")) {
+        $(".modal-header").css("background-color", "rgb(168, 184, 32)");
+      } else if (pokemon.types.includes("water")) {
+        $(".modal-header").css("background-color", "rgb(69, 120, 237)");
+      } else if (pokemon.types.includes("ice")) {
+        $(".modal-header").css("background-color", "rgb(66, 174, 174)");
+      } else if (pokemon.types.includes("electric")) {
+        $(".modal-header").css("background-color", "rgb(252, 234, 161)");
+      } else if (pokemon.types.includes("ground")) {
+        $(".modal-header").css("background-color", "rgb(219, 181, 77)");
+      } else if (pokemon.types.includes("fairy")) {
+        $(".modal-header").css("background-color", "rgb(232, 120, 144)");
+      } else if (pokemon.types.includes("ghost")) {
+        $(".modal-header").css("background-color", "rgb(100, 78, 136)");
+      } else if (pokemon.types.includes("normal")) {
+        $(".modal-header").css("background-color", "rgb(156, 156, 99)");
+      }
+    
+
     modalHeader.append(nameElement);
     modalHeader.append(idNumber)
     modalBody.append(imageElementFront);
@@ -160,3 +187,24 @@ pokemonRepository.loadList().then(function () {
     pokemonRepository.addListItem(pokemon);
   });
 });
+
+function search() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    // li = ul.getElementsByTagName("");
+    li = ul.querySelectorAll(".card");
+    // console.log(li[0].querySelector(".card-body").querySelector(".card-title"));
+    for (i = 0; i < li.length; i++) {
+      // a = li[i].getElementsByTagName("a")[0];
+      a = li[i].querySelector(".card-body").querySelector("button");
+      console.log(a.innerText);
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
